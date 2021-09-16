63ef23f8c7a327897404e4f29f59d0268b55bfdb
Jess70 <patidarjayesh27@gmail.com>
2021-09-16 13:45:56 +0530

    SEO and blogs schema

--- src/component/SEO/seo.schema.js
+++ src/component/SEO/seo.schema.js
@@ -1,30 +1,63 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 let SEO = new Schema(
-	{
-		title:{
-            type:String,
-            unique:true,
-            required:true
-        },
-        titleContent:{
-            type:String,
-            required:true
-        },
-        keywords:{
-            type:String,
-            required:true
-        },
-        description:{
-            type:String,
-            required:true
-        }
+  {
+    title: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    titleContent: {
+      type: String,
+      required: true,
+    },
+    keywords: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    robot: {
+      type: String,
+      // required: true,
+    },
 
-	},
-	{
-		timestamps: true,
-	}
-);
+    og_type: {
+      type: String,
+      // required: true,
+    },
 
-module.exports = mongoose.model("seo", SEO);
+    og_title: {
+      type: String,
+      // required: true,
+    },
+
+    og_description: {
+      type: String,
+      // required: true,
+    },
+
+    og_image: {
+      type: String,
+      // required: true,
+    },
+
+    og_url: {
+      type: String,
+      // required: true,
+    },
+
+    og_siteName: {
+      type: String,
+      // required: true
+    },
+  },
+  {
+    timestamps: true,
+  },
+)
+
+module.exports = mongoose.model('seo', SEO)
